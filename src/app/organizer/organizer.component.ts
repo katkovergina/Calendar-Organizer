bde3161ba8ac01805ebba42d039b840cc5496478
Ekaterina Kovergina <katkovergina@gmail.com>
2022-04-22 12:04:00 +0300

    add some features

--- src/app/organizer/organizer.component.ts
+++ src/app/organizer/organizer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { switchMap } from 'rxjs/operators';
 import { DateService } from '../shared/date.service';
 import { TasksService } from '../shared/tasks.service';
 import { Task } from '../shared/tasks.service';
@@ -12,11 +13,17 @@ import { Task } from '../shared/tasks.service';
 export class OrganizerComponent implements OnInit {
 
   form!: FormGroup
+  tasks: Task[] = []
 
   constructor(public dateService: DateService,
               private tasksService: TasksService) { }
 
   ngOnInit(): void {
+    this.dateService.date.pipe(
+      switchMap(value => this.tasksService.load(value))
+    ).subscribe(tasks => {
+      this.tasks = tasks
+    })
     this.form = new FormGroup({
       title: new FormControl('', Validators.required)
     })
@@ -24,12 +31,20 @@ export class OrganizerComponent implements OnInit {
 
   submit() {
     const {title} = this.form.value
-    console.log(title)
     const task: Task = {
       title,
       date: this.dateService.date.value.format('DD-MM-YY')
     }
-    this.tasksService.create(task).subscribe(task => { this.form.reset() }, err => { console.log('error') })
+    this.tasksService.create(task).subscribe(task => {
+      this.tasks.push(task)
+      this.form.reset() 
+    }, err =>  console.log('error') )
+  }
+
+  remove(task: Task) {
+    this.tasksService.remove(task).subscribe( () =>  {
+      this.tasks = this.tasks.filter(t => t.id !== task.id)
+    }, err => console.log(err))
   }
 
 }
--- src/app/shared/tasks.service.ts
+++ src/app/shared/tasks.service.ts
@@ -2,11 +2,16 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators'
 import { Observable } from "rxjs";
+import * as moment from "moment";
 
 export interface Task {
     id?: string,
     title: string,
     date?: string
+}
+
+interface CreateResponse {
+    name: string
 }
 
 @Injectable({
@@ -18,13 +23,28 @@ export class TasksService {
 
     constructor(private http: HttpClient) {}
 
+    load(date: moment.Moment): Observable<Task[]> {
+        return this.http
+                .get<Task[]>(`${TasksService.url}/${date.format('DD-MM-YY')}.json`)
+                .pipe(map(tasks => {
+                    if (!tasks) {
+                        return []
+                    }
+                    return Object.keys(tasks).map(key => ({...tasks[<any>key], id: key}))
+                }))
+
+    }
+
     create(task: Task): Observable<Task> {
         return this.http
-            .post<any>(`${TasksService.url}/${task.date}.json`, task)
+            .post<CreateResponse>(`${TasksService.url}/${task.date}.json`, task)
             .pipe(map(res => {
-                console.log('Response' + res)
-                return res
+                return {...task, id: res.name}
             }))
+    }
+
+    remove(task: Task): Observable<void> {
+        return this.http.delete<void>(`${TasksService.url}/${task.date}/${task.id}.json`)
     }
 
 }
