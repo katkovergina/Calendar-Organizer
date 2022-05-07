556701d705b9e7a43742e180e76424d6140b7a9c
Ekaterina Kovergina <katkovergina@gmail.com>
2022-05-07 11:36:29 +0300

    add error handler, add login method

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,13 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from "@angular/core";
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+import { onErrorResumeNextStatic } from 'rxjs/internal/operators/onErrorResumeNext';
 
-interface AuthResponseData {
+export interface AuthResponseData {
     kind: string
     idToken: string	
     email: string	
     refreshToken: string
     expiresIn: string
     localId: string
+    registered?: boolean
 }
 
 @Injectable({
@@ -26,6 +30,28 @@ export class AuthService {
                 password: password,
                 returnSecureToken: true
             }
+        ).pipe(catchError(errorRes => {
+            let errorMessage = 'An unknown error occured!'
+            if (!errorRes.error || !errorRes.error.error) {
+                return throwError(errorMessage)
+            }
+            switch (errorRes.error.error.message) {
+                case 'EMAIL_EXISTS':
+                  errorMessage = 'This email exists already'
+            }
+            return throwError(errorMessage)
+        }))
+    }
+
+    login(email: string, password: string) {
+        return this.http.post<AuthResponseData>(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCxMZL-DlmeEFQ5DSi7qe4rto_L1WoCZAY',
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
+            }
         )
+        
     }
 }
